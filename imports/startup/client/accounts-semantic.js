caa6050887e6c10156c71addc3eb2a5dafa08624
RandomPotato26 <random.potato26@gmail.com>
2019-07-05 18:15:17 -0700

    add login handler

---
+++ imports/startup/client/accounts-semantic.js
@@ -0,0 +1,8 @@
+import { AccountsTemplates } from 'meteor/useraccounts:semantic-ui'
+
+AccountsTemplates.configure({
+    defaultTemplate: 'App_home',
+    defaultLayout: 'App_body',
+    defaultLayoutRegions: {},
+    defaultContentRegion: 'main'
+});
