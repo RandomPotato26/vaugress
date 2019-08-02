78530f63afa218a6ba25f23fb6a0483b50b71737
RandomPotato26 <random.potato26@gmail.com>
2019-08-01 17:34:43 -0700

    add logout button

--- imports/startup/both/useraccount.js
+++ imports/startup/both/useraccount.js
@@ -44,7 +44,7 @@ AccountsTemplates.addFields([
         type: 'password',
         required: true,
         minLength: 6,
-        re: /(?=.*\\d)(?=.*[a-z])(?=.*[A-Z]).{6,}/,
+        re: /(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.{6,})/,
         errStr: 'At least 1 digit, 1 lower-case and 1 upper-case',
     },
     {
--- imports/ui/components/accountButton/accountButton.js
+++ imports/ui/components/accountButton/accountButton.js
@@ -1,1 +1,14 @@
 import './accountButton.html';
+
+Template.accountButton.helpers({
+    isLoggedIn: function () {
+        return !!Meteor.user();
+    }
+});
+
+Template.accountButton.events({
+    "click #logout": function (event) {
+        event.preventDefault();
+        Meteor.logout();
+    }
+});
